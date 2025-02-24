d064d9b026c7401c3a32e68ba14cb6281a6fc9af
SeanOmoruyi <C22346841@mytudublin.ie>
2025-02-24 11:58:36 +0000

    Update DiceROLLER.py

--- DiceROLLER.py
+++ DiceROLLER.py
@@ -5,4 +5,8 @@ def dice_roller():
     num_dice = int(input("Enter the number of dice to roll: "))
     results = [random,randint(1,6) for _ in range(num_dice)]
     print("Rolling the dice...")
-    
+    time.sleep(1)
+    print(f"You rollled: {results}")
+
+if __name__ == "__main__":
+    dice_roller()
