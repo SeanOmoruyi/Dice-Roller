1eff70c3036b991a686b51175ef69a2a9657943f
SeanOmoruyi <C22346841@mytudublin.ie>
2025-02-24 11:55:03 +0000

    Update DiceROLLER.py

--- DiceROLLER.py
+++ DiceROLLER.py
@@ -1,2 +1,8 @@
 import random
 import time
+
+def dice_roller():
+    num_dice = int(input("Enter the number of dice to roll: "))
+    results = [random,randint(1,6) for _ in range(num_dice)]
+    print("Rolling the dice...")
+    
